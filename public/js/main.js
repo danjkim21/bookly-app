76b27e2a80c2ff7254ec7b47b221c9cb7d85f51f
danjkim21 <dan.jkim21@gmail.com>
2022-06-24 15:56:23 -0700

    add app button modals draft 1

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+// Google Books API Sample GET Request
+//  https://www.googleapis.com/books/v1/volumes?q=intitle:circe&key=AIzaSyByYEeZn4taw9OfJDef1qCOgAgSDschcaE
+//  `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&key=${API_KEY}`
+
